fe0a50ee82863999489baf5dbec94d8005fae893
Raxxo <Oskar.Hauge123@gmail.com>
2023-10-05 18:22:15 +0200

    Jobbet med backend

--- wishes.php
+++ wishes.php
@@ -7,11 +7,38 @@
     <link rel="stylesheet" href="./style.css">
 </head>
 <body>
-    <h1>Velkommen til min juleønskeside!</h1>
+    <h1 id="Welcome">Her er mine ønsker!</h1>
     <?php
-    $json = file_get_contents("my_data.json");
-    $data = json_decode($json);
-    print_r($data);
+    $oList = array();
+    $jData = file_get_contents("my_data.json");
+    $jInfo = json_decode($jData, true);
+    foreach($jInfo as $x){
+        array_push($oList, $x);
+    }
+    print_r($oList);
+    class Wish {
+        public $Header;
+        public $Desc;
+        public $Price;
+        public $Link;
+        function __construct($Header, $Desc, $Price, $Link){
+            $this->Header = $Header;
+            $this->Desc = $Desc;
+            $this->Price = $Price;
+            $this->Link = $Link;
+        }
+    }
+    function createObj(&$oList, string $Header, string $Desc, float $Price, string $Link){
+        $myObject = new Wish($Header, $Desc, $Price, $Link);
+        array_push($oList, $myObject);
+    }
+    global $encoded;
+    global $sum;
+    $encoded = json_encode($oList);
+    $file = fopen("my_data.json", "w+");
+    fwrite($file, $encoded);
+    fclose($file);
     ?>
+    <br><a href="./index.html">Vil du gå tilbake?</a>
 </body>
 </html>
